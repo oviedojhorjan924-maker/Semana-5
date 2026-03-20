37652c6f864400efd58e2aa5ca0de424ace411c6
oviedojhorjan924-maker <oviedojhorjan924@gmail.com>
2026-03-20 12:55:54 -0500

    Mayor de edad.py

---
+++ Mayor de edad.py
@@ -0,0 +1,19 @@
+#Algoritmo
+#Pedir edad con numero entero
+#Si la edad es mayor o igual 18 puede entrar a la discoteca.
+#Si no es igual o mayor a 18 no puedes ingresar vete para la casa
+
+#Pseudocodigo
+#Inicio
+#Leer numero 
+#Resultado edad>=18
+#Escribir "Puedes ingresar a la discoteca"
+#Resultado <18
+#Escribir "vayase para la casa"
+#Fin
+
+Edad=int(input("dame la edad"))
+if Edad>=18:
+    print("puedes entrar a la discoteca")
+else:
+    print("vayase para la casa")
