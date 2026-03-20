670990f79b8193734e2e6b6b32038e7cf7a09adc
oviedojhorjan924-maker <oviedojhorjan924@gmail.com>
2026-03-20 12:57:25 -0500

    Aprobo o reprobo.py

---
+++ Aprobo o reprobo.py
@@ -0,0 +1,20 @@
+#Algoritmo
+#Digitar nota en decimal
+#Si la nota es mayor o igual 3.0 pasa la materia.
+#Si No pierde la materia.
+
+#Pseudocodigo
+#Inicio
+#Leer numero decimal
+#Resultado >=3.0
+#Escribir"Pasas la materia"
+#Resultado <=3.0
+#Escribir "Pierdes la materia"
+#Fin
+
+
+Nota=float(input("Digita tu nota final"))
+if Nota>=3.0:
+   print("pasas la materia")
+else:
+   print("pierde la materia")
